9ed07638555c282bb26cc2d877cf95a264d44c37
William Lumbantobing <william.sl.tobing@gmail.com>
2021-01-31 20:54:42 +0700

    fix navbar menu button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,8 @@ import ExitImg from "../assets/exit.svg";
 const Navbar = () => {
   const [navButton, setNavButton] = useState(false);
 
+  const showNavigation = () => setNavButton(!navButton);
+
   return (
     <div className="navbar">
       <div className="container">
@@ -17,46 +19,42 @@ const Navbar = () => {
           className="mobile-menu"
           src={MenuImg}
           alt="Open Navigation"
-          onClick={() => setNavButton(true)}
+          onClick={showNavigation}
         />
 
-        {navButton ? (
-          <nav>
-            <img
-              className="mobile-menu-exit"
-              src={ExitImg}
-              alt="Close Navigation"
-              onClick={() => setNavButton(false)}
-            />
+        <nav className={navButton ? "" : "menu-btn"}>
+          <img
+            className="mobile-menu-exit"
+            src={ExitImg}
+            alt="Close Navigation"
+            onClick={showNavigation}
+          />
 
-            <ul className="primary-nav">
-              <li>
-                <a className="current" href="/">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="/">Features</a>
-              </li>
-              <li>
-                <a href="/">Pricing</a>
-              </li>
-            </ul>
+          <ul className="primary-nav">
+            <li>
+              <a className="current" href="/">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/">Features</a>
+            </li>
+            <li>
+              <a href="/">Pricing</a>
+            </li>
+          </ul>
 
-            <ul className="secondary-nav">
-              <li>
-                <a href="/">Contact</a>
-              </li>
-              <li>
-                <a className="go-premium-cta" href="/">
-                  Go Premium
-                </a>
-              </li>
-            </ul>
-          </nav>
-        ) : (
-          ""
-        )}
+          <ul className="secondary-nav">
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <a className="go-premium-cta" href="/">
+                Go Premium
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
