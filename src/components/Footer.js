523fbe34e94c4e986278684398cc65416fc3004d
William Lumbantobing <william.sl.tobing@gmail.com>
2021-02-01 15:42:10 +0700

    adding some transition duration to button

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <section className="footer-section">
+      <div className="container">
+        <h2>Footer</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
