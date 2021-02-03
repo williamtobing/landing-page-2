a45291151e5a966bd8fc8a43ba6bffa68c3cc9a1
William Lumbantobing <william.sl.tobing@gmail.com>
2021-02-03 19:46:52 +0700

    centered hero-content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,22 +7,28 @@ const Hero = () => {
     <section id="to-home" className="hero">
       <div className="container">
         <Zoom>
-          <div className="left-col">
-            <p className="subhead">It's Nitty &amp; Gritty</p>
-            <h1>A Task App That Doesn't Stink</h1>
+          <div className="hero-content">
+            <div className="left-col">
+              <p className="subhead">It's Nitty &amp; Gritty</p>
+              <h1>A Task App That Doesn't Stink</h1>
 
-            <div className="hero-cta">
-              <a href="/" className="primary-cta">
-                Try for free
-              </a>
-              <a href="/" className="watch-video-cta">
-                <img src={WatchImg} alt="Watch a video" />
-                Watch a video
-              </a>
+              <div className="hero-cta">
+                <a href="/" className="primary-cta">
+                  Try for free
+                </a>
+                <a href="/" className="watch-video-cta">
+                  <img src={WatchImg} alt="Watch a video" />
+                  Watch a video
+                </a>
+              </div>
             </div>
-          </div>
 
-          <img className="hero-img" src={IllustrationImg} alt="Illustration" />
+            <img
+              className="hero-img"
+              src={IllustrationImg}
+              alt="Illustration"
+            />
+          </div>
         </Zoom>
       </div>
     </section>
